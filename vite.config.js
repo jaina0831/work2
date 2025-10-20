f14dc7e78b75217c16ae2b307a16642ec8e595c6
jaina0831 <jaina1912@gmail.com>
2025-10-20 10:56:47 +0800

    rename cat-api to api

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // 只在「本機開發」時作用，讓 /api 轉到你的本機 FastAPI 例如 8001
+    proxy: { "/api": "http://127.0.0.1:8001" }
+  }
 })
