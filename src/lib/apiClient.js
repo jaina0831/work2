a61c2e9b52f94f935811742d8f4fcb9317a0364d
jaina0831 <jaina1912@gmail.com>
2025-11-24 11:32:28 +0800

    go

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,6 +1,6 @@
 // src/apiClient.js
 import axios from "axios";
-import { auth } from "./firebase";
+import { auth } from "../firebase";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
